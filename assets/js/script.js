f20f60a23f650fcdf8c7009a925b38ea89a661f4
Haley Seymour <haleycseymour@comcast.net>
2022-04-11 17:19:44 -0700

    boredAPI fetch request and results saved as variables

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,33 +7,79 @@ $(document).ready(function(){
 
 var TypeEl;
 var CostEl;
-var free;
+var free = false;
 var ParticipantsEl;
 var AccessibilityEl;
-var accessibility;
+var accessibility = false;
 
 
 //   click event for find an activity
 $('#findactivity').on("click", function (event) {
   event.preventDefault();
-    TypeEl = $('#type').val();
-    console.log(TypeEl);
-    CostEl = $('#price').val();
-    console.log(CostEl);
-    if (CostEl == 0.0) {
-      free = true;
-    } else {
-      free = false;
-    };
-    console.log(free)
-    ParticipantsEl = $('#participants').val();
-    console.log(ParticipantsEl);
-    AccessibilityEl = $('#accessibility').val();
-    if (AccessibilityEl == 1) {
-      accessibility = true;
-    } else {
-      accessibility = false;
-    }
-    console.log(AccessibilityEl);
-    console.log(accessibility);
+  // pull data from form submission and save as variables
+  TypeEl = $('#type').val();
+  CostEl = $('#price').val();
+  if (CostEl == 0.0) {
+    free = true;
+  } else {
+    free = false;
+  };
+  ParticipantsEl = $('#participants').val();
+  AccessibilityEl = $('#accessibility').val();
+  if (AccessibilityEl == 1) {
+    accessibility = true;
+  } else {
+    accessibility = false;
+  }
+  console.log(TypeEl, free, ParticipantsEl, accessibility);
+  createURL()
 })
+
+function createURL() {
+  var boredURL;
+  // if no parameters are used, set boredURL to random URL
+  if (!TypeEl && !free && !ParticipantsEl && !accessibility) {
+    boredURL = 'http://www.boredapi.com/api/activity/'
+  // if any parameters are provided, set base URL and construct with each parameter
+  } else if (TypeEl || free || ParticipantsEl || accessibility) {
+    boredURL = 'http://www.boredapi.com/api/activity?'
+    // if a type was provided, add query parameter to URL
+    if (TypeEl) {
+      boredURL = boredURL + '&type=' + TypeEl;
+    };
+    // if preference for activity to be free, add query parameter 
+    if (free) {
+      boredURL = boredURL + '&price=0';
+    };
+    // if participants are specified
+    if (ParticipantsEl) {
+      boredURL = boredURL + '&participants=' + ParticipantsEl
+    };
+    // if accessibility is needed
+    if (accessibility) {
+      boredURL = boredURL + '&minaccessibility=0.1';
+    }
+  } 
+  console.log(boredURL)
+  getApi();
+
+  function getApi() {
+    fetch(boredURL)
+      .then(function (response) {
+        if (!response.ok) {
+          throw response.json();
+        }
+      return response.json();  
+      })
+      .then (function(data) {
+        console.log(data)
+        var returnedActivity = data.activity;
+        var returnedType = data.type;
+        var returnedParticipants = data.participants;
+        var returnedPrice = data.price;
+        var returnedAccessibility = data.accessibility;
+        var returnedLink = data.link;
+        console.log(returnedActivity,returnedType,returnedParticipants,returnedPrice,returnedAccessibility,returnedLink)
+      })
+  }
+}
